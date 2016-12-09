55809486725bec0bc1303378e31b503da3118f0f
Darafei Praliaskouski <me@komzpa.net>
2016-12-09 18:11:31 +0300

    tpv[] => jsonb cast

--- sql/types/type_tpv.sql
+++ sql/types/type_tpv.sql
@@ -257,3 +257,27 @@ select ST_Distance(
     p_tpv_2 :: geography
 )
 $$ language 'sql' immutable strict parallel safe;
+
+create or replace function _jsonb_from_tpvarray(
+    p_tpvarray tpv []
+)
+    returns jsonb
+as $$
+select jsonb_agg(
+    jsonb_build_object(
+        'lon', ST_X(p_tpv :: geography :: geometry),
+        'lat', ST_Y(p_tpv :: geography :: geometry),
+        'acc', p_tpv.accuracy,
+        'hdg', p_tpv.heading,
+        'spd', p_tpv.speed,
+        'ts', extract(epoch from p_tpv.ts) * 1000.,
+        'src', p_tpv.source,
+        'osm_id', p_tpv.osm_id
+    )
+)
+from unnest(p_tpvarray) p_tpv
+$$ language 'sql' immutable strict parallel safe;
+
+drop cast if exists ( tpv [] as jsonb );
+create cast ( tpv [] as jsonb )
+with function _jsonb_from_tpvarray(tpv []);
