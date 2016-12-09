d42c427a66de2e384d150dc1e425ea6fb38db5f0
Darafei Praliaskouski <me@komzpa.net>
2016-12-09 19:28:58 +0300

    strip nulls

--- sql/types/type_tpv.sql
+++ sql/types/type_tpv.sql
@@ -263,16 +263,18 @@ create or replace function _jsonb_from_tpvarray(
 )
     returns jsonb
 as $$
-select jsonb_agg(
-    jsonb_build_object(
-        'lon', ST_X(p_tpv :: geography :: geometry),
-        'lat', ST_Y(p_tpv :: geography :: geometry),
-        'acc', p_tpv.accuracy,
-        'hdg', p_tpv.heading,
-        'spd', p_tpv.speed,
-        'ts', extract(epoch from p_tpv.ts) * 1000.,
-        'src', p_tpv.source,
-        'osm_id', p_tpv.osm_id
+select jsonb_strip_nulls(
+    jsonb_agg(
+        jsonb_build_object(
+            'lon', ST_X(p_tpv :: geography :: geometry),
+            'lat', ST_Y(p_tpv :: geography :: geometry),
+            'acc', p_tpv.accuracy,
+            'hdg', p_tpv.heading,
+            'spd', p_tpv.speed,
+            'ts', extract(epoch from p_tpv.ts) * 1000.,
+            'src', p_tpv.source,
+            'osm_id', p_tpv.osm_id
+        )
     )
 )
 from unnest(p_tpvarray) p_tpv
