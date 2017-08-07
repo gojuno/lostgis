0b85b8366f702a8fab5287b68281ec4ed338c436
Darafei Praliaskouski <komzpa@gojuno.com>
2017-08-07 13:32:36 +0300

    test fix

--- test/test_type_opening_hours.sql
+++ test/test_type_opening_hours.sql
@@ -11,7 +11,7 @@ select is(
     overlaps( '2017-08-13 13:00':: timestamp,
               'Mo-Fr 05:00-15:00,19:00-21:00; Sa 05:00-12:00,14:00-21:00; Su 05:00-14:00,17:00-21:00' :: text ::
               opening_hours),
-    false
+    true
 );
 
 select is(
